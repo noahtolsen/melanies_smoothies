d00c391a94e2966756b53c580a44506a3cd9d588
Noah Olsen <noah.t.olsen@gmail.com>
2025-05-09 09:48:48 -0600

    Update streamlit_app.py to use correct connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
+
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
@@ -14,7 +14,8 @@ st.write(
 
 name_on_order = st.text_input("Name on Smoothie:")
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.connection()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 ingredients_list = st.multiselect(
