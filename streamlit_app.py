aaf7245e5503878ba9bae19f5482ce09dd6225a0
Noah Olsen <noah.t.olsen@gmail.com>
2025-05-09 10:50:40 -0600

    Update streamlit_app.py
    show nutrition information based on smoothie recipe. Still not fully functional. Currently shows Watermelon only repeated for however many fruit are in recipe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
+        
     #st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -40,7 +43,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
